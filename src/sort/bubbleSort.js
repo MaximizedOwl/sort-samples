2525464218df07a9394d789834eb8d8ef5e16c7d
MaximizedOwl <mkarasawaaa+0@gmail.com>
2021-05-15 17:55:45 +0900

    バブルソートの処理内容を変更 1. 小さい順にソートされるように変更 2. 一時変数がvarで宣言されているところをletに変更

--- src/sort/bubbleSort.js
+++ src/sort/bubbleSort.js
@@ -1,17 +1,24 @@
-import { check } from "../util/check";
-import { randomNumberGenerator } from "../util/randomNumberGenerator";
+import { check } from '../util/check';
+import { randomNumberGenerator } from '../util/randomNumberGenerator';
 
 export const bubbleSort = () => {
   const randomNumList = randomNumberGenerator();
 
   const startTime = performance.now(); // 開始時間
 
-  for (var i = 0; i < randomNumList.length; i++) {
-    for (var j = randomNumList.length - 1; j > i; j--) {
-      if (randomNumList[j] < randomNumList[j - 1]) {
-        var tmp = randomNumList[j];
-        randomNumList[j] = randomNumList[j - 1];
-        randomNumList[j - 1] = tmp;
+  /* 
+    ソート処理
+  */
+  // (要素数 - 1) 回分、繰り返す
+  for (let i = 0; i < randomNumList.length - 1; i++) {
+    // (要素数 - 1) 回分、繰り返す
+    for (let j = 0; j < randomNumList.length - 1; j++) {
+      // 入れ替えるかどうかの判定。添字が大きいほうが中の数字が小さければswap処理実施。
+      if (randomNumList[j] > randomNumList[j + 1]) {
+        // 添字の大きい数字を一時変数に入れて入れ替える。
+        const tmp = randomNumList[j];
+        randomNumList[j] = randomNumList[j + 1];
+        randomNumList[j + 1] = tmp;
       }
     }
   }
@@ -19,7 +26,8 @@ export const bubbleSort = () => {
   const endTime = performance.now(); // 終了時間
 
   if (check(randomNumList)) {
-    console.log("OK! " + (endTime - startTime));
-    return "OK! " + (endTime - startTime) + " (ms)";
+    console.log('List is... ' + randomNumList);
+    console.log('OK! ' + (endTime - startTime));
+    return 'OK! ' + (endTime - startTime) + ' (ms)';
   }
 };
