469d6b37660274953a38d8404f68e24be2829b7e
MaximizedOwl <mkarasawaaa+0@gmail.com>
2021-05-16 18:25:25 +0900

    判定処理の条件が間違っていたので修正

--- src/util/check.js
+++ src/util/check.js
@@ -1,19 +1,21 @@
-// 1~10000までの配列を生成
+/* 
+  1~10000までの配列を生成
+*/
 const number = 10000;
 let ascNumList = [];
 
+for (let index = 0; index < number; index++) {
+  ascNumList.push(index);
+}
+
+/* 
+  昇順にソートできているかの判定
+  成功：true
+  失敗：false
+*/
 export const check = (sortedList) => {
-  for (let index = 0; index < number; index++) {
-    ascNumList.push(index);
-  }
-
-  /* 
-    昇順にソートしているかの判定
-    成功：true
-    失敗：false
-  */
-
-  for (let index = 0; index < number.length; index++) {
+  for (let index = 0; index < ascNumList.length; index++) {
+    // indexを添字としたときの、昇順リスト（正解）の値とチェック対象の値を突き合わせて、falseになる場合に分岐に入る判定
     if (!(ascNumList[index] === sortedList[index])) {
       return false;
     }
